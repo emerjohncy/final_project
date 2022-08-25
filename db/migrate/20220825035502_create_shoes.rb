eb09c0c1af406f44c69cfe400f4ddbbc66af968d
Emer John Niones <nionesemerjohn@gmail.com>
2022-08-25 12:09:58 +0800

    generate model Shoe

---
+++ db/migrate/20220825035502_create_shoes.rb
@@ -0,0 +1,13 @@
+class CreateShoes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shoes do |t|
+      t.text :condition
+      t.string :color
+      t.string :model
+      t.string :brand
+      t.float :starting_price
+
+      t.timestamps
+    end
+  end
+end
