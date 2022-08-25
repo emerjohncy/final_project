64a0e94f5031fa0eab4a9b9564c2c64f027507a6
Emer John Niones <nionesemerjohn@gmail.com>
2022-08-25 13:06:15 +0800

    generate model Auction

---
+++ db/migrate/20220825041014_create_auctions.rb
@@ -0,0 +1,13 @@
+class CreateAuctions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :auctions do |t|
+      t.references :shoe, null: false, foreign_key: true
+      t.text :description
+      t.datetime :start_date
+      t.datetime :end_date
+      t.string :status, default: "Close"
+
+      t.timestamps
+    end
+  end
+end
