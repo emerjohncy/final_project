cdac556846a8810320214a4345fa46392999e104
Emer John Niones <nionesemerjohn@gmail.com>
2022-08-25 23:06:17 +0800

    add routes for auction,shoes,bid

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :shoes do
+    resources :auctions do
+      resources :bids
+    end
+  end
+  
 end
