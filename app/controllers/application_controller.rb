a3618191378f807dd668e899824e43c58ef2fe68
Emer John Niones <nionesemerjohn@gmail.com>
2022-08-27 12:41:59 +0800

    transfer configuration to corresponding controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  # protected
 
-  def configure_permitted_parameters
-    added_attrs = [:username, :first_name, :last_name, :contact_number, :email, :password, :password_confirmation, :remember_me]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-  end
+  # def configure_permitted_parameters
+  #   added_attrs = [:username, :first_name, :last_name, :contact_number, :email, :password, :password_confirmation, :remember_me]
+  #   devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  #   devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+  #   devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  # end
 end
