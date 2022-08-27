93028bdd9d6789f9b6a70acf5d3dddd576266688
Emer John Niones <nionesemerjohn@gmail.com>
2022-08-27 12:56:46 +0800

    add configuration params

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_permitted_parameters, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,10 +19,15 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+  end
+
 end
