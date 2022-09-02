556c83243c58e639ba5dff35fcb6eafac5e5d2e1
BryBTG <50550050+BryBTG@users.noreply.github.com>
2022-09-02 20:26:39 +0800

    Revert "Add / Setup Gem for design"
    This reverts commit dee83711a464ed1d3119efcfb5448e6aa0cd402b.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,3 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link_tree ../builds
-//= link application.css
-//= link_tree ../builds
