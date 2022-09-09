7bc530a75c022dfa8ff946a3b78f1c09208ab016
Emer John Niones <nionesemerjohn@gmail.com>
2022-09-09 19:46:42 +0800

    add mailer when bid is placed successfully

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,4 +5,12 @@ class UserMailer < ApplicationMailer
     @user = params[:user]
     mail(to: @user.email, subject: 'Welcome to AucShoes!')
   end
+
+  def bid_success
+    @bid = params[:bid]
+    @owner = @bid.auction.user
+    @bidder = @bid.user
+    mail(to: @owner.email, subject: "AucShoes Bid Success")
+  end
+  
 end
